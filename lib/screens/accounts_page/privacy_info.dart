e84ec07e554707566fad1dec87bd18799439186e
hawks <devicathira1991@gmail.com>
2023-12-14 12:33:36 +0530

    commit

--- lib/screens/accounts_page/privacy_info.dart
+++ lib/screens/accounts_page/privacy_info.dart
@@ -2,26 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class PrivacyInfo extends StatefulWidget {
-  const PrivacyInfo({super.key});
+  const PrivacyInfo({Key? key}) : super(key: key);
 
   @override
   State<PrivacyInfo> createState() => _PrivacyInfoState();
 }
 
 class _PrivacyInfoState extends State<PrivacyInfo> {
-  final String websiteUrl = "https://hotelaryas.com/privacy_policy";
 
-  Future<void> _launchWebsite() async {
+  final String privacyPolicyUrl = "https://hotelaryas.com/privacy_policy";
+  final String termsConditionUrl = "https://hotelaryas.com/terms_and_conditions";
+  final String refundPolicyUrl = "https://hotelaryas.com/refund_policy";
+  final String shippingAndDeliveryPolicyUrl = "https://hotelaryas.com/shipping_policy";
+  final String contactUsUrl = "https://hotelaryas.com/contact_us";
+  final String aboutUsUrl = "https://hotelaryas.com";
+
+  Future<void> _launchUrl(String url) async {
     try {
-      if (await canLaunch(websiteUrl)) {
-        await launch(websiteUrl);
-      } else {
-        throw 'Could not launch $websiteUrl';
+      final bool nativeLaunch = await launch(
+        url,
+        forceWebView: false,
+        enableJavaScript: true,
+      );
+
+      if (!nativeLaunch) {
+        throw 'Could not launch $url';
       }
     } catch (e) {
       print('Error launching URL: $e');
     }
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -31,61 +42,35 @@ class _PrivacyInfoState extends State<PrivacyInfo> {
       ),
       body: ListView(
         children: [
-          Divider(
-            thickness: 1,
-          ),
+          Divider(thickness: 1),
           ListTile(
-            onTap: _launchWebsite,
+            onTap: () => _launchUrl(privacyPolicyUrl),
             title: Text("Privacy Policy", style: TextStyle(fontSize: 18)),
           ),
-          Divider(
-            thickness: 1,
-          ),
+          Divider(thickness: 1),
           ListTile(
-            onTap: () {},
+            onTap: () => _launchUrl(termsConditionUrl),
             title: Text("Terms & Conditions", style: TextStyle(fontSize: 18)),
           ),
-          Divider(
-            thickness: 1,
-          ),
+          Divider(thickness: 1),
           ListTile(
-            onTap: () {},
+            onTap: () => _launchUrl(refundPolicyUrl),
             title: Text("Cancellation & Refund Policy", style: TextStyle(fontSize: 18)),
           ),
-          Divider(
-            thickness: 1,
-          ),
+          Divider(thickness: 1),
           ListTile(
-            onTap: () {},
-            title: Text("Disclaimer Policy", style: TextStyle(fontSize: 18)),
-          ),
-          Divider(
-            thickness: 1,
-          ),
-          ListTile(
-            onTap: () {},
+            onTap: () => _launchUrl(shippingAndDeliveryPolicyUrl),
             title: Text("Shipping & Delivery Policy", style: TextStyle(fontSize: 18)),
           ),
-          Divider(
-            thickness: 1,
-          ),
+          Divider(thickness: 1),
           ListTile(
-            onTap: () {},
+            onTap: () => _launchUrl(contactUsUrl),
             title: Text("Contact Us", style: TextStyle(fontSize: 18)),
           ),
-          Divider(
-            thickness: 1,
-          ),
+          Divider(thickness: 1),
           ListTile(
-            onTap: () {},
+            onTap: () => _launchUrl(aboutUsUrl),
             title: Text("About Us", style: TextStyle(fontSize: 18)),
-          ),
-          Divider(
-            thickness: 1,
-          ),
-          ListTile(
-            onTap: () {},
-            title: Text("Privacy Policy", style: TextStyle(fontSize: 18)),
           ),
         ],
       ),
